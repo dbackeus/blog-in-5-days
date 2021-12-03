833967c8b2f4cd7e2048c58c42a8d4d30ed925fe
David Backeus <duztdruid@gmail.com>
2021-12-03 20:48:09 +0100

    Keep alive support (21000 req/sec!)

--- blog.rb
+++ blog.rb
@@ -4,6 +4,7 @@ server = TCPServer.new 3000
 def respond(socket, body: "", status: 200)
   socket.print "HTTP/1.1 #{status}\r\n"
   socket.print "Content-Type: text/html\r\n"
+  socket.print "Content-Length: #{body.bytesize}\r\n"
   socket.print "\r\n"
   socket.print body
 end
@@ -23,16 +24,31 @@ routes = {
   }
 }
 
+def extract_request(socket)
+  first_line = socket.gets
+  return unless first_line # client is done requesting over the keep alive connection
+
+  method, path, http_version = first_line.split(" ")
+  rest = ""
+  while (line = socket.gets) && (line != "\r\n")
+    rest << line
+  end
+  [method, path, rest]
+end
+
+
 while socket = server.accept
-  request = socket.gets
+  loop do
+    request = extract_request(socket)
+    break unless request
 
-  method, path, http_version = request.split(" ")
+    method, path, rest = request
 
-  _regex, lambda = routes[method].find { |regex, _lambda| path[regex] }
+    _regex, lambda = routes[method]&.find { |regex, _lambda| path[regex] }
 
-  status, body = lambda ? lambda.call : [404, STATIC_VIEWS.fetch("404.html")]
+    status, body = lambda ? lambda.call : [404, STATIC_VIEWS.fetch("404.html")]
 
-  respond(socket, status: status, body: body)
-
+    respond(socket, status: status, body: body)
+  end
   socket.close
 end
