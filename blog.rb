5802c8bc0668a236254b52b04d2c9a87e26b0a52
David Backeus <duztdruid@gmail.com>
2021-12-03 15:49:12 +0100

    Some kind of router

--- blog.rb
+++ blog.rb
@@ -8,10 +8,31 @@ def respond(socket, body: "", status: 200)
   socket.print body
 end
 
+STATIC_VIEWS = Dir.glob("app/views/*").each_with_object({}) do |path, hash|
+  hash[File.basename(path)] = File.read(path)
+end
+
+routes = {
+  "GET" => {
+    %r{/$} => lambda do
+      [200, STATIC_VIEWS.fetch("index.html")]
+    end,
+    %r{/posts/?$} => lambda do
+      [200, STATIC_VIEWS.fetch("posts.html")]
+    end,
+  }
+}
+
 while socket = server.accept
   request = socket.gets
 
-  respond(socket, body: File.read("app/views/index.html"))
+  method, path, http_version = request.split(" ")
+
+  _regex, lambda = routes[method].find { |regex, _lambda| path[regex] }
+
+  status, body = lambda ? lambda.call : [404, STATIC_VIEWS.fetch("404.html")]
+
+  respond(socket, status: status, body: body)
 
   socket.close
 end
