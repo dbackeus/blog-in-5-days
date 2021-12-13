a021efa894acd0340b375090544167615b7df0fe
David Backeus <duztdruid@gmail.com>
2021-12-13 15:21:14 +0100

    Introduce Request object (need a way to pass route params)

--- blog.rb
+++ blog.rb
@@ -4,6 +4,38 @@ require "zlib"
 require_relative "forked_web_server"
 require_relative "threaded_web_server"
 
+class Request
+  CARRIAGE_RETURN = "\r\n"
+
+  def self.from_socket(socket)
+    first_line = socket.gets
+    return unless first_line # client is done requesting over the keep alive connection
+
+    method, path, http_version = first_line.split(" ")
+    content_length = nil
+    headers = {}
+    while (line = socket.gets) && (line != CARRIAGE_RETURN)
+      key, value = line.split(":")
+      headers[key.strip.downcase] = value.strip
+    end
+    if content_length = headers["content-length"]
+      body = socket.readpartial(content_length.to_i)
+    end
+
+    new(method: method, path: path, headers: headers, body: body)
+  end
+
+  attr_reader :method, :path, :headers, :body
+  attr_accessor :route_match
+
+  def initialize(method:, path:, headers:, body:)
+    @method = method
+    @path = path
+    @headers = headers
+    @body = body
+  end
+end
+
 STATIC_VIEWS = Dir.glob("app/views/*").each_with_object({}) do |path, hash|
   view = File.read(path)
   hash[File.basename(path)] = view
@@ -12,45 +44,26 @@ end
 
 routes = {
   "GET" => {
-    %r{/$} => lambda do |body|
+    %r{/$} => lambda do |request|
       [200, STATIC_VIEWS.fetch("index.html.deflated")]
     end,
-    %r{/large/?$} => lambda do |body|
+    %r{/large/?$} => lambda do |request|
       [200, STATIC_VIEWS.fetch("large.html.deflated")]
     end,
-    %r{/posts/?$} => lambda do |body|
+    %r{/posts/?$} => lambda do |request|
       [200, STATIC_VIEWS.fetch("posts.html.deflated")]
     end,
-    %r{/posts/new$} => lambda do |body|
+    %r{/posts/new$} => lambda do |request|
       [200, STATIC_VIEWS.fetch("posts_new.html.deflated")]
     end,
   },
   "POST" => {
-    %r{/posts/?$} => lambda do |body|
-      puts body
+    %r{/posts/?$} => lambda do |request|
+      puts request.body
       [404, STATIC_VIEWS.fetch("404.html.deflated")]
     end,
   }
 }
-
-CARRIAGE_RETURN = "\r\n"
-
-def extract_request(socket)
-  first_line = socket.gets
-  return unless first_line # client is done requesting over the keep alive connection
-
-  method, path, http_version = first_line.split(" ")
-  content_length = nil
-  headers = {}
-  while (line = socket.gets) && (line != CARRIAGE_RETURN)
-    key, value = line.split(":")
-    headers[key.strip.downcase] = value.strip
-  end
-  if content_length = headers["content-length"]
-    body = socket.readpartial(content_length.to_i)
-  end
-  [method, path, headers, body]
-end
 
 def respond(socket, body: "", status: 200)
   socket.print(
@@ -69,14 +82,13 @@ puts "Starting server on port 3000"
 server = ThreadedWebServer.new
 server.start(number_of_workers: 8) do |socket|
   loop do
-    request = extract_request(socket)
+    request = Request.from_socket(socket)
     break unless request
 
-    method, path, headers, body = request
+    _regex, lambda = routes[request.method]&.find { |regex, _lambda| request.path[regex] }
 
-    _regex, lambda = routes[method]&.find { |regex, _lambda| path[regex] }
+    status, body = lambda&.call(request) || [404, STATIC_VIEWS.fetch("404.html.deflated")]
 
-    status, body = lambda ? lambda.call(body) : [404, STATIC_VIEWS.fetch("404.html.deflated")]
 
     respond(socket, status: status, body: body)
   end
