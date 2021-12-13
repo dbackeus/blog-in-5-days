4ac802cd1fa2cba832dfcff965a770f37638d3b0
David Backeus <duztdruid@gmail.com>
2021-12-13 10:47:45 +0100

    Proper forking including shutdown handling

--- blog.rb
+++ blog.rb
@@ -1,8 +1,6 @@
 require 'socket'
 require "zlib"
 
-server = TCPServer.new 3000
-
 STATIC_VIEWS = Dir.glob("app/views/*").each_with_object({}) do |path, hash|
   view = File.read(path)
   hash[File.basename(path)] = view
@@ -47,24 +45,45 @@ def respond(socket, body: "", status: 200)
   socket.flush
 end
 
-loop do
-  socket = server.accept
-  socket.sync = false
+puts "Starting server on port 3000"
+server = TCPServer.new 3000
+
+worker_pids = 10.times.map do
   fork do
     loop do
-      request = extract_request(socket)
-      break unless request
+      socket = server.accept
+      socket.sync = false
+      loop do
+        request = extract_request(socket)
+        break unless request
 
-      method, path, rest = request
+        method, path, rest = request
 
-      _regex, lambda = routes[method]&.find { |regex, _lambda| path[regex] }
+        _regex, lambda = routes[method]&.find { |regex, _lambda| path[regex] }
 
-      status, body = lambda ? lambda.call : [404, STATIC_VIEWS.fetch("404.html.deflated")]
+        status, body = lambda ? lambda.call : [404, STATIC_VIEWS.fetch("404.html.deflated")]
 
-      respond(socket, status: status, body: body)
+        respond(socket, status: status, body: body)
+      end
+    rescue Errno::ECONNRESET, Interrupt
+    ensure
+      socket&.close
     end
-  rescue Errno::ECONNRESET
-  ensure
-    socket.close
   end
+end
+
+shutdown = false
+
+Signal.trap("TERM") { shutdown = true }
+Signal.trap("QUIT") { shutdown = true }
+Signal.trap("INT") { shutdown = true }
+
+loop do
+  if shutdown
+    puts "Shutting down"
+    worker_pids.each { |pid| Process.kill("TERM", pid) }
+    worker_pids.each { |pid| Process.wait(pid) }
+    exit
+  end
+  sleep 1
 end
